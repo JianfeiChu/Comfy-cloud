6e6cdc644a434bcde91649088832732e021a78ae
JianfeiChu <33381568+JianfeiChu@users.noreply.github.com>
2024-10-11 18:10:17 +0100

    Update default1.sh

--- default1.sh
+++ default1.sh
@@ -53,7 +53,7 @@ CHECKPOINT_MODELS=(
 	"https://huggingface.co/stabilityai/stable-diffusion-xl-base-1.0/resolve/main/sd_xl_base_1.0.safetensors"
 	"https://huggingface.co/stabilityai/stable-diffusion-xl-refiner-1.0/resolve/main/sd_xl_refiner_1.0.safetensors"
         # Flux1 dev
-	"https://huggingface.co/stabilityai/stable-diffusion-xl-base-1.0/resolve/main/sd_xl_base_1.0.safetensors"
+
 )
 
 UNET_MODELS=(
